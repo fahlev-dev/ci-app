76042cc72b528aab18377e695613e43007dde40b
Reza Fahlevi <rfahlevi501@gmail.com>
2022-07-01 16:38:28 +0700

    menambahkan tombol cari, detail, dan ubah

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -19,11 +19,31 @@
 
 <div class="row mt-3">
     <div class="col-md-6">
+        <form action="" method="POST">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Cari data mahasiswa.." name="keyword">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit">Button</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<div class="row mt-3">
+    <div class="col-md-6">
     <ul class="list-group">
         <h3>Daftar Mahasiswa</h3>
+        <?php if(empty($mahasiswa)) : ?>
+            <div class="alert alert-danger" role="alert">
+                Data mahasiswa tidak ditemukan.
+            </div>
+        <?php endif; ?>
         <?php foreach($mahasiswa as $mhs) : ?>
             <li class="list-group-item"><?= $mhs['nama']; ?>
                 <a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right" onclick="return confirm('yakin?');">Hapus</a>
+                <a href="<?= base_url(); ?>mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge badge-success float-right">Ubah</a>
+                <a href="<?= base_url(); ?>mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right">Detail</a>
             </li>
         <?php endforeach; ?>
     </ul>
